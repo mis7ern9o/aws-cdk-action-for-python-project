bcfd36a71722c1ff820f868dcc6e3dc0bdfa82da
David ROBIN <david.a.robin@gmail.com>
2020-11-22 19:25:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 set -e
-
-cd "${GITHUB_WORKSPACE}"/"${INPUT_WORKING_DIR}"
+echo "Building your layer"
+sh -c "${INPUT_PYTHON_BUILD_COMMAND}"
+sh -c "${INPUT_ZIP_COMMAND}"
+echo "Running your CDK command"
+cd "${GITHUB_WORKSPACE}"/"${INPUT_CDK_WORK_DIR}"
 sh -c "npm ci"
 sh -c "${INPUT_CDK_COMMAND}"
