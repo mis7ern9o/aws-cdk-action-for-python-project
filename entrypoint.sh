c0c5b412cd81c171778b287bfb2dfc4b21a25691
David ROBIN <david.a.robin@gmail.com>
2020-11-22 15:16:36 +0100

    Update entrypoint.sh
    full cdk command to allow a cdk local or global

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 pip3 install -r requirements.txt
-cd ${GITHUB_WORKSPACE}/${INPUT_WORKING_DIR}
+cd "${GITHUB_WORKSPACE}"/"${INPUT_WORKING_DIR}"
 sh -c "npm ci"
-sh -c "cdk ${INPUT_CDK_COMMAND}"
+sh -c "${INPUT_CDK_COMMAND}"
