204737346725e65e0fa558065979a9d0ac124176
David ROBIN <david.a.robin@gmail.com>
2020-11-23 21:05:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
+service docker start
 set -e
 echo "Building your layer"
-sh -c "${INPUT_PYTHON_BUILD_COMMAND}"
-sh -c "${INPUT_ZIP_COMMAND}"
+#sh -c "${INPUT_PYTHON_BUILD_COMMAND}"
+#sh -c "${INPUT_ZIP_COMMAND}"
 echo "Running your CDK command"
 echo "region ${AWS_DEFAULT_REGION}"
 cd "${GITHUB_WORKSPACE}"/"${INPUT_CDK_WORK_DIR}"
